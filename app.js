ce55dba2e526ac66f8f71dc1fbea5f513ff93244
Ch Preetham <chpreetham322@gmail.com>
2018-03-25 20:57:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mysql = require('mysql');
+const SHA256 = require("crypto-js/sha256");
+//var server = require('http').createServer(app);  
+var io = require('socket.io-client')//(server);
+
+app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + 'socket.io-client/dist/socket.io')); 
 var bodyParser = require("body-parser");
 var con = mysql.createConnection({
     host: "localhost",
@@ -9,6 +15,13 @@ var con = mysql.createConnection({
   password: "mysql",
   database: "login"
 })
+  con.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
 
 var port = process.env.PORT || 3000;
  app.use(bodyParser.urlencoded({
@@ -19,15 +32,76 @@ app.get('/',function(req, res){
   res.sendFile(path.join(__dirname + '/public/login.html'));
 
 });
+var hash=" ";
 
-app.post('/myaction',function(req,res){
-    con.connect(function(err){
-if(!err) {
-    console.log("Database is connected ... nn");
-} else {
-    console.log("Error connecting database ... nn");
-}
+ 
+ 
+
+ 
+ 
+app.post('/myaction2',function(req,res){
+var v_id = req.body.voter_id;
+hash =  SHA256( JSON.stringify(v_id) ).toString();
+//console.log(hash);
+var socket = io.connect('http://192.168.1.11:3000');
+ socket.on('connect', function(data) {
+  socket.emit('join', hash);
 });
+ 
+socket.on('broad',function(data){
+   console.log(data);
+   
+   con.query('SELECT * FROM log2 WHERE hash=?',[data],function(error,results,fields){
+    if(error){ 
+      res.send({
+        "code":400,
+        "failed":"Some error occured"
+      });
+    }
+    else{
+        console.log('The solution is: ', results);
+        if(results.length ==0)
+        {   //var record = [ [ data ]]; 
+         /*   con.connect(function(err){
+  console.log('Connection established');
+  if(err){
+      console.log('Error connecting to Db');
+      return;
+  } else*/ 
+                  
+    var query = con.query('INSERT INTO log2 SET hash=?',data,function(error,results){
+      if(error) console.log(error);
+        console.log('db added');
+        console.log(query.sql);
+        //con.end();
+    });
+           
+             res.sendFile(path.join(__dirname + '/public/Voting_recorded.html'));
+             socket.destroy();
+       }
+        
+        
+    else if(results.length!=0){
+      res.send({
+        "code":204,
+        "success":"Stop Cheating Start Living"
+          }); socket.destroy();
+        }
+    }
+  });
+ 
+
+ });
+
+
+});
+
+
+
+
+//myaction 
+app.post('/myaction',function(req,res){
+  
    var uname = req.body.user;
    var pass = req.body.pwd;
     
@@ -41,7 +115,7 @@ if(!err) {
        });
   }
   else{
-     console.log('The solution is: ', results);
+    // console.log('The solution is: ', results);
         if(results.length >0)
         {
              res.sendFile(path.join(__dirname + '/public/voting_page.html'));
@@ -57,7 +131,46 @@ if(!err) {
   }
   });
 
-con.end();
+//con.end();
 });
+ 
+
+/*io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+     
+        //console.log(data);
+        client.broadcast.emit('broad',hash);
+
+
+    client.on('broad',function(data){
+      con.query('SELECT * FROM log2 WHERE hash=?',[hash], function (error, results, fields) {
+        if(error) {
+    // console.log("error ocurred",error);
+       res.send({
+      "code":400,
+      "failed":"error ocurred"
+       });
+  }
+  else{
+    // console.log('The solution is: ', results);
+        if(results.length ==0)
+        {    con.query('INSERT INTO log2 VAULES(hash)',function(){});
+             res.sendFile(path.join(__dirname + '/public/Voting_recorded.html'));
+        }
+        
+        
+    else{
+      res.send({
+        "code":204,
+        "success":"Stop Cheating Start Living"
+          });
+        }
+  }
+      });
+    });
+});*/
+//app.listen(port);
+ 
  
 app.listen(port);
